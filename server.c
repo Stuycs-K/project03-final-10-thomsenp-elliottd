955a3fdbef65c873198d941ccc4d5045385e0251
Daniel Elliott <delliott40@stuy.edu>
2024-01-09 15:37:48 -0500

    fixed server plan, modified proposal to have realistic plan

--- server.c
+++ server.c
@@ -1,4 +1,16 @@
-//num of players and difficulty as input
+int main(){
+    int counter = 0;
+    int playerCOunt
+
+    printf("Please type the number of expected players and the difficulty as a number 1-10(Ex: 5 5 is 5 players at average difficulty)");
+    printf("At any time after the first player joins you can start the game by typing start");
+    sscanf()//pull sockets get player count, get difficulty
+    fd_set read_fds;
+    int[playerCount + 1];
+
+
+
+}
 //select -> create sockets for each player -> up to num of players
 //start game pulling faction/talent data from players
 //send all players starting gold, troops, and cities(based on diffuclty, factions, and talents)
